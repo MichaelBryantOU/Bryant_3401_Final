003e2902c9399057d0cb5e5713596a6e4b3a82de
MichaelBryantOU <mb281416@scc-mmlab-13.local>
2019-05-02 13:03:31 -0400

    Entire Project Upload

---
+++ Bryant_3401_Final/Assets/Scripts/AgentMotor.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AgentMotor : MonoBehaviour
+{
+    public float Speed = 5;
+    public NavMeshAgent agent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+
+            if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 100))
+            {
+                agent.speed = Speed;
+                agent.destination = hit.point;
+            }
+        }
+    }
+}
